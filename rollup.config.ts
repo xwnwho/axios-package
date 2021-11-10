2f94c5409e70b2f50f0ecad56031da9dabf00486
yhj <1520590201@qq.com>
2021-11-10 15:13:26 +0800

    feat: 使用@rollup/plugin-typescript插件打包

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,13 +1,10 @@
+import { defineConfig } from 'rollup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-// import typescript from '@rollup/plugin-typescript'
-// import dts from "rollup-plugin-dts"
-
-import typescript from 'rollup-plugin-typescript2'
-
+import typescript from '@rollup/plugin-typescript'
 import pkg from './package.json'
 
-export default {
+export default defineConfig({
   input: 'src/index.ts',
   output: [
     {
@@ -33,7 +30,7 @@ export default {
     nodeResolve(),
     commonjs(),
     typescript({
-      useTsconfigDeclarationDir: true,
+      tsconfig: './tsconfig.json',
     }),
   ],
-}
+})
